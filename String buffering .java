1953cf9fafcce05a0640840e7ae9a546984f6430
KARTHIKA  N <karthikanagaraj2006@gmail.com>
2026-07-26 21:49:18 +0530

    Create String buffering .java

---
+++ String buffering .java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+
+        sb.append(" World");
+        System.out.println(sb);
+
+        sb.insert(5, " Java");
+        System.out.println(sb);
+
+        sb.replace(6, 10, "C");
+        System.out.println(sb);
+
+        sb.delete(5, 7);
+        System.out.println(sb);
+
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
