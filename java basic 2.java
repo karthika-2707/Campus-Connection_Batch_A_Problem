8dac392d4161ce0fb7a5ec42fc52c9e9b00e40b7
KARTHIKA  N <karthikanagaraj2006@gmail.com>
2026-07-26 21:31:40 +0530

    Create java basic 2.java

---
+++ java basic 2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+    public static void main (string[]args)
+    {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.next().charAt(0);
+        System.out.println(args[0]);
+    }
+}
